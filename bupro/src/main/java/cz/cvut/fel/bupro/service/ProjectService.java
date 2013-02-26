1ab446e8a185a2d978e86016e483981bc691c120
Viktor Souček <viksoucek@gmail.com>
2013-02-26 18:19:30 +0100

    Add transaction annotations, remove mock impl

--- bupro/src/main/java/cz/cvut/fel/bupro/service/ProjectService.java
+++ bupro/src/main/java/cz/cvut/fel/bupro/service/ProjectService.java
@@ -1,49 +1,50 @@
 package cz.cvut.fel.bupro.service;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import cz.cvut.fel.bupro.dao.ProjectRepository;
-import cz.cvut.fel.bupro.model.Authorship;
-import cz.cvut.fel.bupro.model.Comment;
 import cz.cvut.fel.bupro.model.Project;
-import cz.cvut.fel.bupro.model.User;
 
 @Service
 public class ProjectService {
 
+	private final Log log = LogFactory.getLog(getClass());
+
 	@Autowired
 	private ProjectRepository projectRepository;
-	
+
+	@Transactional
 	public List<Project> getAllProjects() {
-		User u = new User();
-		u.setFirstName("Karel");
-		u.setLastName("Vomacka");
-		Project p1 = new Project();
-		p1.setId(0L);
-		p1.setName("Test 1");
-		p1.setAuthorship(new Authorship(u));
-		Comment comment = new Comment();
-		comment.setAuthorship(new Authorship(u));
-		comment.setTitle("Some extra notes");
-		comment.setText("Dont forget to get extra reward for this project");
-		p1.add(comment);
-		u = new User();
-		u.setFirstName("Venca");
-		u.setLastName("Rohlik");
-		Project p2 = new Project();
-		p2.setId(1L);
-		p2.setName("Test 2");
-		p2.setAuthorship(new Authorship(u));
-		return Arrays.asList(p1, p2);
+		log.info("get all projects");
+		return projectRepository.findAll();
 	}
 
+	@Transactional
 	public Project getProject(Long id) {
-		int index = id.intValue();
-		return getAllProjects().get(index);
+		log.info("get project " + id);
+		Project project = projectRepository.findOne(id);
+		project.getMemberships().size(); // force fetch
+		project.getComments().size(); // force fetch
+		project.getTags().size(); // force fetch
+		return project;
 	}
-	
+
+	@Transactional
+	public Project save(Project project) {
+		if (project.getId() != null) {
+			Project p = projectRepository.findOne(project.getId());
+			p.setName(project.getName());
+			p.setDescription(project.getDescription());
+			return p;
+		} else {
+			return projectRepository.save(project);
+		}
+	}
+
 }
