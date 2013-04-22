ac07f8bfdcca90ff445580129e2446a4aa2ca78c
Viktor Souček <viksoucek@gmail.com>
2013-04-22 17:16:55 +0200

    fix script after KOS client move

--- scripts/generate-jaxb.sh
+++ scripts/generate-jaxb.sh
@@ -1,5 +1,5 @@
 #
 # Script to generate JAXB for KOS
 #
-project=bupro
+project=kos-client
 xjc -d ../$project/src/main/java/ -p cz.cvut.fel.kos.jaxb ../$project/src/main/resources/jaxb/kos.xsd -b ../$project/src/main/resources/jaxb/bindings.xjb
