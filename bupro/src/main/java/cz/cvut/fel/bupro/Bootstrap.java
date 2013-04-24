0c467848c935aa273deca913088e909694119f54
Viktor Souček <viksoucek@gmail.com>
2013-04-24 20:05:39 +0200

    Enable port resolving for server

--- bupro/src/main/java/cz/cvut/fel/bupro/Bootstrap.java
+++ bupro/src/main/java/cz/cvut/fel/bupro/Bootstrap.java
@@ -13,12 +13,13 @@ public class Bootstrap {
 		Tomcat tomcat = new Tomcat();
 
 		String webPort = System.getenv("PORT");
-		// if (webPort == null || webPort.isEmpty()) {
-		webPort = "8080";
-		// }
+
+		if (webPort == null || webPort.isEmpty()) {
+			webPort = "8080";
+		}
 		tomcat.setPort(Integer.valueOf(webPort));
 
-		StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());		
+		StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
 //		tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
 		System.out.println("configuring app with basedir: " + new File(webappDirLocation).getAbsolutePath());
 		
