9b9c216af4d6bebaed822bf2b59ede4cb69b2c5c
Viktor Souček <viksoucek@gmail.com>
2013-04-25 18:01:27 +0200

    Bootstrap clean, prepare for SSL

--- bupro/src/main/java/cz/cvut/fel/bupro/Bootstrap.java
+++ bupro/src/main/java/cz/cvut/fel/bupro/Bootstrap.java
@@ -2,29 +2,57 @@ package cz.cvut.fel.bupro;
 
 import java.io.File;
 
+import org.apache.catalina.Service;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.tomcat.util.scan.StandardJarScanner;
 
 public class Bootstrap {
 
+	private static int getenvInt(String propertyName, int defaultValue) {
+		String value = System.getenv(propertyName);
+		if (value == null || value.trim().isEmpty()) {
+			return defaultValue;
+		}
+		return Integer.parseInt(value);
+	}
+
 	public static void main(String[] args) throws Exception {
 		String webappDirLocation = "src/main/webapp/";
 		Tomcat tomcat = new Tomcat();
 
-		String webPort = System.getenv("PORT");
-
-		if (webPort == null || webPort.isEmpty()) {
-			webPort = "8080";
+		int httpPort = getenvInt("PORT", 8080);
+		tomcat.setPort(httpPort);
+		boolean ssl = false;
+		if (ssl) {
+			// TODO implement SSL
+			int httpsPort = getenvInt("HTTPS_PORT", 8443);
+			String keyAlias = "";
+			String password = "";
+			String keystorePath = "";
+			Connector httpsConnector = new Connector();
+			httpsConnector.setPort(httpsPort);
+			httpsConnector.setSecure(true);
+			httpsConnector.setScheme("https");
+			httpsConnector.setAttribute("keyAlias", keyAlias);
+			httpsConnector.setAttribute("keystorePass", password);
+			httpsConnector.setAttribute("keystoreFile", keystorePath);
+			httpsConnector.setAttribute("clientAuth", "false");
+			httpsConnector.setAttribute("sslProtocol", "TLS");
+			httpsConnector.setAttribute("SSLEnabled", true);
+			Service service = tomcat.getService();
+			service.addConnector(httpsConnector);
+			Connector defaultConnector = tomcat.getConnector();
+			defaultConnector.setRedirectPort(httpsPort);
 		}
-		tomcat.setPort(Integer.valueOf(webPort));
-
 		StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
-//		tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+		// tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
 		System.out.println("configuring app with basedir: " + new File(webappDirLocation).getAbsolutePath());
-		
-		//declare an alternate location for your "WEB-INF/classes" dir:
-		((StandardJarScanner) ctx.getJarScanner()).setScanAllDirectories(true); //FIXME Scan all - devel only 
+
+		// declare an alternate location for your "WEB-INF/classes" dir:
+		// FIXME Scanall - devel only
+		((StandardJarScanner) ctx.getJarScanner()).setScanAllDirectories(true);
 
 		tomcat.start();
 		tomcat.getServer().await();
