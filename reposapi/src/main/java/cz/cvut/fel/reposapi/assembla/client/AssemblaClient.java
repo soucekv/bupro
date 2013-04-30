497d5ead2c37d77f5426851316c9f40f6ad9069a
Viktor Souček <viksoucek@gmail.com>
2013-04-30 16:50:56 +0200

    remove unused imports

--- reposapi/src/main/java/cz/cvut/fel/reposapi/assembla/client/AssemblaClient.java
+++ reposapi/src/main/java/cz/cvut/fel/reposapi/assembla/client/AssemblaClient.java
@@ -1,13 +1,10 @@
 package cz.cvut.fel.reposapi.assembla.client;
 
-import java.net.HttpURLConnection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
