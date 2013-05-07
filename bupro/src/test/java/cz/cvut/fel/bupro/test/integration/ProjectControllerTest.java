045b6ad4f4231a3a147798ee3e2cfc83f52d9982
Viktor Souček <viksoucek@gmail.com>
2013-05-07 21:26:36 +0200

    Add membership integration tests

--- bupro/src/test/java/cz/cvut/fel/bupro/test/integration/ProjectControllerTest.java
+++ bupro/src/test/java/cz/cvut/fel/bupro/test/integration/ProjectControllerTest.java
@@ -26,9 +26,14 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
+import cz.cvut.fel.bupro.TimeUtils;
 import cz.cvut.fel.bupro.dao.ProjectRepository;
+import cz.cvut.fel.bupro.dao.UserRepository;
+import cz.cvut.fel.bupro.model.Membership;
+import cz.cvut.fel.bupro.model.MembershipState;
 import cz.cvut.fel.bupro.model.Project;
 import cz.cvut.fel.bupro.model.ProjectCourse;
+import cz.cvut.fel.bupro.model.User;
 import cz.cvut.fel.bupro.test.configuration.AbstractControllerTest;
 import cz.cvut.fel.kos.KosSemesterCode;
 import cz.cvut.fel.kos.Period;
@@ -42,6 +47,9 @@ public class ProjectControllerTest extends AbstractControllerTest {
 
 	@Autowired
 	private ProjectRepository projectRepository;
+
+	@Autowired
+	private UserRepository userRepository;
 
 	@Autowired
 	private RequestMappingHandlerAdapter handlerAdapter;
@@ -77,6 +85,44 @@ public class ProjectControllerTest extends AbstractControllerTest {
 
 	private ProjectCourse getCourseInstance() {
 		return getProjectInstnace().getCourse();
+	}
+
+	private User getUserInstance() {
+		return userRepository.findByUsername("vomacka");
+	}
+
+	private Project createProject() throws Exception {
+		MockHttpServletRequest request = new MockHttpServletRequest("POST", "/project/save");
+		request.setContentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+		request.setParameter("id", "");
+		request.setParameter("name", "Test Project");
+		request.setParameter("course", String.valueOf(getCourseInstance().getId()));
+		request.setParameter("startSemester", KosSemesterCode.encode(2013, Period.WINTER));
+		request.setParameter("endSemester", KosSemesterCode.encode(2013, Period.WINTER));
+		request.setParameter("capacity", String.valueOf(1));
+		request.setParameter("description", "test description");
+		request.setParameter("autoApprove", "true");
+		request.setParameter("tagGroup", "Software");
+		request.setParameter("tags", "Java, JSF, JPA");
+		request.setParameter("repository.repositoryProvider", "GITHUB");
+		request.setParameter("repository.repositoryUser", "soucekv");
+		request.setParameter("repository.repositoryName", "bupro");
+		request.setParameter("repository.applicationId", "");
+		request.setParameter("repository.applicationSecret", "");
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		Object handler = handlerMapping.getHandler(request).getHandler();
+		ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
+		Assert.assertEquals(200, response.getStatus());
+		Assert.assertTrue(modelAndView.getViewName().contains("redirect:/project/view/"));
+		Project project = (Project) modelAndView.getModelMap().get("project");
+		Assert.assertNotNull(project.getId());
+		Membership membership = new Membership();
+		membership.setCreated(TimeUtils.createCurrentTimestamp());
+		membership.setChanged(membership.getCreated());
+		membership.setProject(project);
+		membership.setUser(getUserInstance());
+		project.getMemberships().add(membership);
+		return project;
 	}
 
 	@Test
@@ -128,6 +174,7 @@ public class ProjectControllerTest extends AbstractControllerTest {
 		request.setParameter("endSemester", KosSemesterCode.encode(2013, Period.WINTER));
 		request.setParameter("capacity", String.valueOf(1));
 		request.setParameter("description", "test description");
+		request.setParameter("autoApprove", "true");
 		request.setParameter("tagGroup", "Software");
 		request.setParameter("tags", "Java, JSF, JPA");
 		request.setParameter("repository.repositoryProvider", "GITHUB");
@@ -142,6 +189,46 @@ public class ProjectControllerTest extends AbstractControllerTest {
 		Assert.assertTrue(modelAndView.getViewName().contains("redirect:/project/view/"));
 		Project project = (Project) modelAndView.getModelMap().get("project");
 		Assert.assertNotNull(project.getId());
+	}
+
+	@Test
+	@Transactional
+	public void testApproveProjectMember() throws Exception {
+		Project project = createProject();
+		Membership membership = project.getMemberships(MembershipState.WAITING_APPROVAL).iterator().next();
+		User user = membership.getUser();
+		mockMvc.perform(get("/project/membership/approve?projectId=" + project.getId() + "&userId=" + user.getId()))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("project"))
+			.andExpect(model().attribute("project", project))
+			.andExpect(view().name("project-view"))
+			.andExpect(content().string(new Contains(project.getName())));
+	}
+
+	@Test
+	@Transactional
+	public void testDeclineProjectMember() throws Exception {
+		Project project = createProject();
+		Membership membership = project.getMemberships(MembershipState.WAITING_APPROVAL).iterator().next();
+		User user = membership.getUser();
+		mockMvc.perform(get("/project/membership/decline?projectId=" + project.getId() + "&userId=" + user.getId()))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("project"))
+			.andExpect(model().attribute("project", project))
+			.andExpect(view().name("project-view"))
+			.andExpect(content().string(new Contains(project.getName())));
+	}
+
+	@Test
+	@Transactional
+	public void testJoinProject() throws Exception {
+		Project project = getProjectInstnace();
+		mockMvc.perform(get("/project/join/" + project.getId()))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("project"))
+			.andExpect(model().attribute("project", project))
+			.andExpect(view().name("project-view"))
+			.andExpect(content().string(new Contains(project.getName())));
 	}
 
 }
